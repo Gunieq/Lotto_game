a36a7e72693bd6a2f4083c4e9ab9cdb924c405e1
Dominik Czorniej <gunieq@gmail.com>
2021-06-05 15:58:06 +0200

    Finished work

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Lotto_game
+
+## Rules
+1. User should provide six numbers
+   - Every number should be different from another
+   - User should provide numbers in range 1-49
+2. User will try to guess the random generated numbers
+3. Ask to play again
+
+### TODO
+1. Create a random list in range 1-49
+2. Take 6 numbers from first list and sort it
+3. Take user input that has 6 numbers, make it a list and sort
+4. Compare and print results
--- lotto.py
+++ lotto.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def drawn_numbers():  # function for drawing random 6 numbers without duplicates
+    nums_array = random.sample(range(1, 50), 49)
+    random_arr = []
+    random_arr_temp = []
+    while len(random_arr) < 6:
+        random_arr_temp.append(random.choice(nums_array))
+        for elem in random_arr_temp:
+            if elem not in random_arr:
+                random_arr.append(elem)
+
+    return sorted(random_arr)
 
 
-# Press the green button in the gutter to run the script.
+def user_numbers():  # drawing 6 different numbers from user
+    print("Podaj sześć liczb w zakresie 1-49")
+    user_array = []
+    i = 1
+    while i < 7:
+        try:
+            user_input = int(input(f"Podaj {i} liczbę: "))
+            if user_input not in range(1, 50):
+                raise TypeError("Błędny zakres liczb!")
+        except ValueError:
+            raise ValueError("Błędny typ danych")
+        for elem in user_array:
+            if user_input in user_array:
+                raise TypeError("Dubel !")
+        user_array.append(user_input)
+        i += 1
+    return sorted(user_array)
+
+
+def print_numbers(numbers):  # function for printing results
+    print(", ".join(str(number) for number in sorted(numbers)))
+
+
+def lotto():  # main app func
+    user_nums = user_numbers()
+    print("Numbers you picked: ")
+    print_numbers(user_nums)
+    random_nums = drawn_numbers()
+    print("Lotto numbers: ")
+    print_numbers(random_nums)
+
+    counter = 0
+    for i in user_nums:
+        if i in random_nums:
+            counter += 1
+    print(f"You guessed {counter} numbers !")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    lotto()
